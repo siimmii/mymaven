f126cbe13c216ed22fb62ca8b863dcc28113b601
akhil <akhil@Akhilash>
2023-02-02 22:33:33 +0530

    seccond commit

--- vTiger/src/main/java/Generic_utility/BaseClass.java
+++ vTiger/src/main/java/Generic_utility/BaseClass.java
@@ -19,6 +19,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 	
 	//comment from git humb
+	//viewed comment
 	public WebDriver driver;
 	@BeforeSuite(groups = {"smoke","regression"})
 	public void BS()
